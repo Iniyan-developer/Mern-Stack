b35528a9df505af5b93d2bdfd4358121f0089c9f
iniyan-developer <iniyaveliniyan@gmail.com>
2026-05-30 11:42:14 +0530

    Adding day-02 js

---
+++ js/day-02.js
@@ -0,0 +1,36 @@
+alert("Hai Welcome");
+
+confirm("Are you Sure");
+
+
+let students={
+    name:"Priyadharshan",
+    Age:22,
+    Gender:"Male",
+    Role:"Junior Software Developer"
+}
+
+console.log(students);
+console.table(students);
+
+
+// variables
+var a=10;
+console.log(a);
+
+
+
+
+
+var a=100;
+console.log(a);
+
+
+let as=10;
+
+console.log(as);
+
+
+
+let as=100;
+console.log(as);
